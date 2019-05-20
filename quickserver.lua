f878785f513816df35a663cb591cd9fae40208f9
zach "valrym <valrymhops@gmail.com>
2019-05-19 22:33:08 -0400

    fix it to actually work

--- quickserver.lua
+++ quickserver.lua
@@ -1,5 +1,5 @@
 local settings = {
-	["prefix"] = "&"
+	["prefix"] = "@"
 }
 
 local servers = {
@@ -9,23 +9,19 @@ local servers = {
 
 function quickserver(m)
 		if m:GetID() == 6 or m:GetID() == 5 and m:GetInt( 1 ) == client.GetLocalPlayerIndex() then 
-			if string.find(m:GetString(4,1), settings.prefix) then
-				local msgs = m:GetString(4,1);
-				local params = {}
-				for sub in msgs:gmatch("%S+") do table.insert(params, sub) end
-				if params[2] == nil then return end
-				local cmd = params[2] .. " " .. fullparams
-				if params[2] == "server" then 
+			local msgs = m:GetString(4,1);
+			local params = {}
+			for sub in msgs:gmatch("%S+") do table.insert(params, sub) end
+			if params[2] == "server" then 
 					if params[3] == "*" then
 						for i,v in next, servers do
 							client.Command("echo" .. " [CPS]: " .. i, true)
 						end
 					return
 					end 
-				client.Command("connect " .. servers[params[3]], true); 
+					local sparam = params[3]; client.Command("connect " .. servers[sparam], true); 
 					return 
 				end
-			end
 		end
 end
 
